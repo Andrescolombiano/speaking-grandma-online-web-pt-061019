b2eaaa76a0196968e54d263451ba75eb259366f4
Andres Carvajal <jc1993box@hotmail.com>
2019-06-28 15:53:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,8 @@
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
-# NO, NOT SINCE 1938!
+NO, NOT SINCE 1938!
+# 
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
